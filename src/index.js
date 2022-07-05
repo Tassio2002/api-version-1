156a8abefd3f0f58fadeddad5d0da27e606ae602
tassio.santos2002@gmail.com <tassio.santos2002@gmail.com>
2022-07-05 20:54:36 -0300

    Subindo função middleware que verifica o tipo de usuário e permite ou não a execução das operações

--- src/index.js
+++ src/index.js
@@ -27,10 +27,10 @@ const pool = new Pool({
 function verifyJWT(req, res, next) {
     const token = req.headers['x-access-token']
 
-    if(!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
+    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' })
 
-    jwt.verify(token, process.env.SECRET, function(err, decoded) {
-        if(err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token' })
+    jwt.verify(token, process.env.SECRET, function (err, decoded) {
+        if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token' })
         req.user_id = decoded.id
         next()
     })
@@ -38,19 +38,19 @@ function verifyJWT(req, res, next) {
 
 //login
 app.post('/login', (req, res) => {
-    if(req.body.user_email === "admin@gmail.com" && req.body.password === "admin123456") {
+    if (req.body.user_email === "admin@gmail.com" && req.body.password === "admin123456") {
         const { user_id } = req.body
-        const token = jwt.sign({user_id}, process.env.SECRET, {
-            expiresIn: 900
+        const token = jwt.sign({ user_id }, process.env.SECRET, {
+            expiresIn: 1600
         })
         return res.json({ auth: true, token: token })
     }
 
-    res.status(500).json({message: "Login inválido"})
+    res.status(500).json({ message: "Login inválido" })
 })
 
 //Endpoint raiz
-app.get('/', (req, res) => {console.log('Olá node')})
+app.get('/', (req, res) => { console.log('Olá node') })
 
 //Pega todos os usuários {só host}
 app.get('/users', async (req, res) => {
@@ -71,7 +71,7 @@ app.post('/signup', async (req, res) => {
     let user = ''
     try {
         user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [user_email])
-        if(!user.rows[0]) {
+        if (!user.rows[0]) {
             user = await pool.query('INSERT INTO users (user_type, user_email, password, session_expiration) VALUES ($1, $2, $3, $4) RETURNING *', [user_type, user_email, password, session_expiration])
         }
         return res.status(200).send(user.rows)
@@ -79,66 +79,45 @@ app.post('/signup', async (req, res) => {
         return res.status(400).send(err)
     }
 })
-let isAdmin
-app.use('/authors/:user_id', async function (req, res, next) {
+
+app.use('/authors/user/:user_id', async function (req, res, next) {
     const { user_id } = req.params
     try {
         const userType = await pool.query('SELECT user_type FROM users WHERE user_id = ($1)', [user_id])
 
         if (userType.rows[0].user_type == "admin") {
             console.log('passou')
-            //isAdmin = true, next()
-            return res.status(200)
+            return res.status(200), next()
         } else {
             console.log('não passou')
-            //isAdmin = false, next()
             return res.status(500).send({
                 message: 'This operation can only be done by an admin user.'
             })
         }
     } catch (err) {
         return res.status(400).send(err)
-    } 
-})
-
-//Criar uma função
-//Apagar este endpoint
-app.get('/test/:user_id', async (req, res) => {
-    const { user_id } = req.params
-    try {
-        const test = await pool.query('SELECT user_type FROM users WHERE user_id = ($1)', [user_id])
-        res.status(200).send(test.rows)
-        
-        if (test.rows[0].user_type == "admin") {
-            return console.log('funcionou')
-        } else {
-            return console.log(test.rows[0])
-        }
-    } catch (err) {
-        return res.status(400).send(err)
     }
 })
+
 //Cria um novo author {Só admin}
-app.post('/authors/:user_id',verifyJWT, async (req, res) => {
-    const { user_id } = req.params
-    try { 
-            const { author_name } = req.body
-            const { user_id } = req.body
-            const newAuthor = await pool.query('INSERT INTO authors (author_name, user_id) VALUES ($1, $2) RETURNING *', [author_name, user_id])
+app.post('/authors/user/:user_id', verifyJWT, async (req, res) => {
+    const { user_id } = req.params//apagar
+    try {
+        const { author_name } = req.body
+        const { user_id } = req.body
+        const newAuthor = await pool.query('INSERT INTO authors (author_name, user_id) VALUES ($1, $2) RETURNING *', [author_name, user_id])
 
-            return res.status(200).send(newAuthor.rows)
-
-        
+        return res.status(200).send(newAuthor.rows)
     } catch (err) {
         return res.status(500).send({
-        message: ''
-    })
-}
+            message: ''
+        })
+    }
 })
 
 
 //Pega todos os autores de um id especifico
-app.get('/authors/:user_id', verifyJWT, async (req, res) => {
+app.get('/authors/user/:user_id', verifyJWT, async (req, res) => {
     const { user_id } = req.params
     try {
         const allAuthors = await pool.query('SELECT * FROM authors WHERE user_id = ($1)', [user_id])
@@ -151,15 +130,15 @@ app.get('/authors/:user_id', verifyJWT, async (req, res) => {
 
 //Atualiza author {Só admin}
 //Fazer apenas uma query
-app.patch('/author/:user_id/:author_id', verifyJWT, async (req,res) => {
+app.patch('/author/:user_id/:author_id', verifyJWT, async (req, res) => {
     const { user_id, author_id } = req.params
     const data = req.body
     try {
         const belongsToUser = await pool.query('SELECT * FROM authors WHERE user_id = ($1) AND author_id = ($2)', [user_id, author_id])
         if (!belongsToUser.rows[0]) return res.status(400).send('Operation not allowed, this author does not belong to this user.')
-        
-        const updateAuthor = await pool.query('UPDATE authors SET author_name = ($1) WHERE author_id = ($2) RETURNING *', 
-        [data.author_name, author_id])
+
+        const updateAuthor = await pool.query('UPDATE authors SET author_name = ($1) WHERE author_id = ($2) RETURNING *',
+            [data.author_name, author_id])
         return res.status(200).send(updateAuthor.rows)
     } catch (err) {
         return res.status(400).send(err)
@@ -188,8 +167,8 @@ app.post('/paper/:user_id/:author_id', verifyJWT, async (req, res) => {
     const { paper_title, paper_summary } = req.body
     const { author_id } = req.body //trocar por author_id
     try {
-        const newPaper = await pool.query('INSERT INTO papers (paper_title, paper_summary, author_id) VALUES ($1, $2, $3) RETURNING *', 
-        [paper_title, paper_summary, author_id])
+        const newPaper = await pool.query('INSERT INTO papers (paper_title, paper_summary, author_id) VALUES ($1, $2, $3) RETURNING *',
+            [paper_title, paper_summary, author_id])
         return res.status(200).send(newPaper.rows)
     } catch (err) {
         return res.status(400).send(err)
@@ -210,16 +189,16 @@ app.get('/papers/:author_id', verifyJWT, async (req, res) => {
 //Atualiza paper por autor {Só admin}
 //Permitir que o usuário consiga alterar apenas alguns dados
 //Verificar se o autor existe no banco de dados em caso de o usuário querer atualizar o autor
-app.patch('/paper/:user_id/:author_id/:paper_id', verifyJWT, async (req,res) => {
+app.patch('/paper/:user_id/:author_id/:paper_id', verifyJWT, async (req, res) => {
     const { author_id, paper_id } = req.params
     const data = req.body
     try {
         const belongsToUser = await pool.query('SELECT * FROM papers WHERE author_id = ($1) AND paper_id = ($2)', [author_id, paper_id])
         if (!belongsToUser.rows[0]) return res.status(400).send('Operation not allowed, this paper does not belong to this author.')
 
-        const updatePaper = await pool.query('UPDATE papers SET paper_title = ($1), paper_summary = ($2), author_id = ($3) WHERE paper_id = ($4) RETURNING *', 
-        [data.paper_title, data.paper_summary, data.author_id, paper_id])
-        
+        const updatePaper = await pool.query('UPDATE papers SET paper_title = ($1), paper_summary = ($2), author_id = ($3) WHERE paper_id = ($4) RETURNING *',
+            [data.paper_title, data.paper_summary, data.author_id, paper_id])
+
         return res.status(200).send(updatePaper.rows)
     } catch (err) {
         return res.status(400).send(err)
