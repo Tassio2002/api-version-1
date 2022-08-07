35f2931ca53a6895806d26232f379e0df6f1c92a
tassio.santos2002@gmail.com <tassio.santos2002@gmail.com>
2022-08-06 22:07:29 -0300

    Ajustando o envio do JSON como resposta

--- src/Controllers/AuthorController.js
+++ src/Controllers/AuthorController.js
@@ -1,10 +1,22 @@
 const pool = require('../Database/DBConnection')
 
 const createAuthor = async (req, res) => {
-    const { author_name, user_id } = req.body
+    const { author_name, user_id, picture } = req.body
     try {
-        const newAuthor = await pool.query('INSERT INTO authors (author_name, user_id) VALUES ($1, $2) RETURNING *', [author_name, user_id])
-        return res.status(201).send(newAuthor.rows)
+        const newAuthor = await pool.query('INSERT INTO authors (author_name, user_id, picture) VALUES ($1, $2, $3) RETURNING *', [author_name, user_id, picture])
+        
+        let authorId = newAuthor.rows[0].author_id
+        let id = newAuthor.rows[0].id
+        let authorName = newAuthor.rows[0].author_name
+        let pictureURL = newAuthor.rows[0].picture
+        return res.status(201).json({
+            "id": id,
+            "author":{
+                "author_id": authorId,
+                "author_name": authorName,
+                "picture":pictureURL
+            }
+        })
     } catch (err) {
         return res.status(500).send({
             message: ''
@@ -12,14 +24,42 @@ const createAuthor = async (req, res) => {
     }
 }
 
-const getAuthors = async (req, res) => {
+const getAuthors = async (req, res, next) => {
     const { user_id } = req.params
     try {
-        const allAuthors = await pool.query('SELECT * FROM authors WHERE user_id = ($1)', [user_id])
-        if (!allAuthors.rows[0]) {
+        const authors = await pool.query('SELECT * FROM authors WHERE user_id = ($1)', [user_id])
+        let authorsCount = authors.rowCount
+        
+        if (!authors.rows[0]) {
             return res.status(404).send({message: 'This user has no authors'})
         } else {
-            return res.status(200).send(allAuthors.rows)
+            //Colocar cada resultado num array e iterar
+            
+            let authorId = authors.rows[0].author_id
+            let id = authors.rows[0].id
+            let authorName = authors.rows[0].author_name
+            let pictureURL = authors.rows[0].picture
+
+            const allAuthors = []
+
+            for (let i = 0; i < authorsCount; i++) {
+                authorId = authors.rows[i].author_id
+                id = authors.rows[i].id
+                authorName = authors.rows[i].author_name
+                pictureURL = authors.rows[i].picture
+
+                let json = {
+                    "id": id,
+                    "author":{
+                        "author_id": authorId,
+                        "author_name": authorName,
+                        "picture":pictureURL
+                    }
+                }
+                allAuthors.push(json)
+            }
+
+            return res.status(200).json(allAuthors)
         }
     } catch (err) {
         return res.status(400).send(err)
--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -3,8 +3,6 @@ let { convertSecondsToTime, expirationTime } = require('../Services/session-expi
 const { verifyJWT } = require('../Services/api-key')
 const jwt = require('jsonwebtoken')
 
-
-
 //Pega o tempo de expiração da chave de API e faz uma contagem regressiva
 let interval = setInterval(() => {
     expirationTime -= 1
@@ -13,6 +11,55 @@ let interval = setInterval(() => {
 
     convertSecondsToTime()
 }, 1000)
+
+const signup = async (req,res) => {
+    const { user_name, user_email, user_type, password } = req.body
+    
+    try {
+        const user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [user_email])
+        if (!user.rows[0]) {
+            const createUser = await pool.query('INSERT INTO users (user_name, user_type, user_email, password) VALUES ($1, $2, $3, $4) RETURNING *', [user_name, user_type, user_email, password])
+            return res.status(201).send(createUser.rows)
+        }else {
+            return res.status(400).send({
+                message: 'A user with this email already exists in the system, please enter your email and password'
+            })
+        }
+        
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+}
+
+const login = async (req, res) => {
+    const { user_id, user_email, password } = req.body
+    try {
+        const loginData = await pool.query('SELECT user_email, password FROM users WHERE user_email LIKE ($1) AND password LIKE ($2)',
+        [user_email, password])
+        console.log(loginData.rows[0])
+        if (!loginData.rows[0]) {
+            return res.status(401).send({message: 'Invalid login, incorrect email or password, please try again'})
+        } else {
+            const token = jwt.sign({ user_id }, 'secret', {
+                expiresIn: expirationTime
+            })
+            return res.json({ auth: true, token: token })
+        }
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+}
+
+const profile = async (req, res) => {
+    const { user_id } = req.params
+    try {
+        await pool.query('UPDATE users SET session_expiration = ($1) WHERE user_id = ($2)', [convertSecondsToTime(expirationTime), user_id])
+        const profile = await pool.query('SELECT * FROM users WHERE user_id = ($1)', [user_id])
+        return res.status(200).send(profile.rows)
+    } catch (err) {
+        return res.status(500).send(err)
+    }
+}
 
 const getAllUsers = async (req, res) => {
     try {
@@ -48,61 +95,13 @@ const deleteUser = async (req, res) => {
     }
 }
 
-const signup = async (req,res) => {
-    const { user_name, user_email, user_type, password } = req.body
-    
-    try {
-        const user = await pool.query('SELECT * FROM users WHERE user_email = ($1)', [user_email])
-        if (!user.rows[0]) {
-            const createUser = await pool.query('INSERT INTO users (user_name, user_type, user_email, password) VALUES ($1, $2, $3, $4) RETURNING *', [user_name, user_type, user_email, password])
-            return res.status(201).send(createUser.rows)
-        }else {
-            return res.status(400).send({
-                message: 'A user with this email already exists in the system, please enter your email and password'
-            })
-        }
-        
-    } catch (err) {
-        return res.status(500).send(err)
-    }
-}
-
-const profile = async (req, res) => {
-    const { user_id } = req.params
-    try {
-        await pool.query('UPDATE users SET session_expiration = ($1) WHERE user_id = ($2)', [convertSecondsToTime(expirationTime), user_id])
-        const profile = await pool.query('SELECT * FROM users WHERE user_id = ($1)', [user_id])
-        return res.status(200).send(profile.rows)
-    } catch (err) {
-        return res.status(500).send(err)
-    }
-}
-
-const login = async (req, res) => {
-    const { user_id, user_email, password } = req.body
-    try {
-        const loginData = await pool.query('SELECT user_email, password FROM users WHERE user_email LIKE ($1) AND password LIKE ($2)',
-        [user_email, password])
-        console.log(loginData.rows[0])
-        if (!loginData.rows[0]) {
-            return res.status(401).send({message: 'Invalid login, incorrect email or password, please try again'})
-        } else {
-            const token = jwt.sign({ user_id }, 'secret', {
-                expiresIn: expirationTime
-            })
-            return res.json({ auth: true, token: token })
-        }
-    } catch (err) {
-        return res.status(500).send(err)
-    }
-}
 
 
 module.exports = {
+    signup,
+    login,
+    profile,
     getAllUsers,
     patchUser,
-    deleteUser,
-    signup,
-    profile,
-    login
+    deleteUser
 }
